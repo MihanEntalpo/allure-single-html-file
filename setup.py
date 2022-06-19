f6a5b05fc101c1adfd56353fc61752ba17ed5d3e
MihanEntalpo <mihanentalpo@yandex.ru>
2022-06-19 18:48:12 +0700

    new readme file uploaded to pip package

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def read(fname):
 
 setup(
     name=PACKAGE_NAME,
-    version='1.0.4',
+    version='1.0.5',
     author='MihanEntalpo, Sine.wang',
     author_email='mihanentalpo@yandex.ru, sinecelia.wang@gmail.com',
     maintainer='Sine.wang',
--- versions.md
+++ versions.md
@@ -1,5 +1,7 @@
 # Versions and changes
 
+## 1.0.5. New readme file :)
+
 ## 1.0.4. Finally made version that successfully installs with pip
 
 ## 1.0.0. Initial working version
